dddf8f644e76e5d3821fdce2ee6c626774d83a93
David I. Lehn <dlehn@digitalbazaar.com>
2024-06-05 22:19:10 -0400

    Initial files.

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
 # RFC 9285 Base45 Encoder/Decoder _(@digitalbazaar/base45)_
+
+> Encoder/decoder for [The Base45 Data Encoding][] for [Node.js][] and Web browsers
+
+## Table of Contents
+
+- [Background](#background)
+- [Install](#install)
+- [Usage](#usage)
+- [Security](#security)
+- [Contribute](#contribute)
+- [Commercial Support](#commercial-support)
+- [License](#license)
+
+## Background
+
+This library provides an encoder and decoder for RFC 9285 [The Base45 Encoding
+Scheme][] using an alphabet designed for efficient use in QR codes using
+Alphanumeric mode. It works in both [Node.js][] and in Web browsers with no
+dependencies.
+
+## Install
+
+- Node.js >=18 is supported.
+
+To install for [Node.js][] or in a Web project using npm:
+
+```
+npm install @digitalbazaar/base45
+```
+
+To install locally or for development:
+
+```
+git clone https://github.com/digitalbazaar/base45.git
+cd base45
+npm install
+```
+
+## Usage
+
+```js
+import {encode, decode} from '@digitalbazaar/base45';
+```
+
+### Encoding
+
+* `encode(input])`
+  * **`input`**: `Uint8Array` - bytes to encode
+  * Returns a base45 encoded string.
+
+```js
+import {encode} from '@digitalbazaar/base45';
+
+const input1 = Uint8Array([1, 2, 3, 4]);
+const encoded1 = encode(input1);
+// > X507H0
+```
+
+### Decoding
+
+* `decode(input)`
+  * **`input`**: `String` - string to decode
+  * Returns a `Uint8Array` with the decoded bytes.
+
+```js
+import {decode} from '@digitalbazaar/base45';
+
+const input2 = 'X507H0';
+const decoded2 = decode(input2);
+// > Uint8Array [ 1, 2, 3, 4 ]
+```
+
+### String Handling
+
+This library uses [Uint8Array][] for encoder input and decoder output.  If
+conversion to and from strings is needed it can be done with a variety of tools
+such as [TextEncoder][] and [TextDecoder][] using the [Encoding][] standard.
+
+```js
+const input3 = new TextEncoder().encode('abc');
+const encoded3 = encode(input3);
+// > 0EC92
+
+const decoded4 = decode(encoded3);
+const output4 = new TextDecoder().decode(decoded4);
+// > abc
+```
+
+## Security
+
+- Processing large inputs could result in high resource usage.
+
+## Contribute
+
+Please follow the [Bedrock contributing
+guidelines](https://github.com/digitalbazaar/bedrock/blob/master/CONTRIBUTING.md).
+
+PRs accepted.
+
+If editing the README, please conform to the
+[standard-readme](https://github.com/RichardLitt/standard-readme)
+specification.
+
+## Commercial Support
+
+Commercial support for this library is available upon request from
+Digital Bazaar: support@digitalbazaar.com
+
+## License
+
+[New BSD License (3-clause)](LICENSE) © Digital Bazaar
+
+[Encoding]: https://encoding.spec.whatwg.org/
+[Node.js]: https://nodejs.org/
+[TextDecoder]: https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder
+[TextEncoder]: https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+[The Base45 Data Encoding]: https://www.rfc-editor.org/rfc/rfc9285
+[Uint8Array]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array
---
+++ tests/api.spec.js
@@ -0,0 +1,193 @@
+/*!
+ * Copyright (c) 2020-2024 Digital Bazaar, Inc. All rights reserved.
+ */
+import chai from 'chai';
+import chaiBytes from 'chai-bytes';
+chai.use(chaiBytes);
+const should = chai.should();
+
+import {decode, encode} from '../lib/index.js';
+
+// https://www.rfc-editor.org/rfc/rfc9285
+// test from the spec
+const specVectors = [
+  [
+    'AB',
+    'BB8'
+  ],
+  [
+    'Hello!!',
+    '%69 VD92EX0'
+  ],
+  [
+    'base-45',
+    'UJCLQE7W581'
+  ],
+  [
+    'ietf!',
+    'QED8WEX0'
+  ],
+];
+
+// misc tests
+const miscVectors = [
+  [[], ''],
+  ['', ''],
+  ['a', '72'],
+  ['ab', '0EC'],
+  ['abc', '0EC92'],
+  [[0x00], '00'],
+  [[0x00, 0x00], '000'],
+];
+
+// tests from README.md
+const readmeVectors = [
+  [[1, 2, 3, 4], 'X507H0'],
+  ['abc', '0EC92'],
+];
+
+const testVectors = [
+  ...specVectors,
+  ...miscVectors,
+  ...readmeVectors
+].map(([data, result]) => [_toUint8Array(data), result]);
+
+const invalidEncodeVectors = [
+  [0, TypeError],
+  [123456, TypeError],
+  [true, TypeError],
+  [3.14159, TypeError],
+  [[], TypeError],
+  [[0, 1, 2], TypeError],
+  [{}, TypeError]
+];
+
+// tables from index file, keep in sync
+const encodeTable = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:';
+const decodeTable = new Uint8Array(256);
+
+// build decode table, using 0xFF as invalid value.
+decodeTable.fill(0xFF);
+for(let i = 0; i < encodeTable.length; ++i) {
+  decodeTable[encodeTable.charCodeAt(i)] = i;
+}
+
+// invalid alphabet chars
+const invalidChars = [...[...decodeTable].entries()]
+  .filter(v => v[1] === 0xFF)
+  .map(v => [`00${String.fromCharCode(v[0])}`, RangeError]);
+
+// invalid lengths
+const invalidLengths = [
+  ['0', RangeError],
+  ['0000', RangeError],
+];
+
+// invalid decoded size
+const invalidSizes = [
+  // check main triplet size
+  ['GGW', RangeError], // 0x10000 / 65536 too large
+  [':::', RangeError], // 0x163f5 / 91125 too large
+  // check trailing triplet size
+  ['000V5', RangeError], // 0x100 / 256 too large
+  ['000::', RangeError], // 0x7e8 / 2024 too large
+];
+
+// misc invalid
+const invaildMisc = [
+  // every char decode position checks validity
+  ['\xff00', RangeError],
+  ['0\xff0', RangeError],
+  ['00\xff', RangeError],
+  ['000\xff0', RangeError],
+  ['0000\xff', RangeError],
+];
+
+const invalidDecodeVectors = [
+  ...invalidChars,
+  ...invalidLengths,
+  ...invalidSizes,
+  ...invaildMisc
+];
+
+describe('base45-universal APIs', () => {
+  describe('encode', () => {
+    it('should properly encode', async () => {
+      /* eslint-disable-next-line no-unused-vars */
+      const results = testVectors.map(([input, _]) => {
+        return [
+          input,
+          encode(input)
+        ];
+      });
+      results.forEach((r, i) => {
+        should.exist(r);
+        should.exist(r[0], 'result should exist');
+        should.exist(r[1], 'expected result should exist');
+        r[0].should.equalBytes(testVectors[i][0]);
+        r[1].should.equal(testVectors[i][1]);
+      });
+    });
+    it('throws errors on invalid input', async () => {
+      invalidEncodeVectors.forEach(([input, expectedError]) => {
+        let result;
+        let error;
+        try {
+          result = encode(input);
+        } catch(e) {
+          error = e;
+        }
+        should.not.exist(result);
+        should.exist(error);
+        (error instanceof expectedError).should.be.true;
+      });
+    });
+  }); // end encode
+
+  describe('decode', () => {
+    it('should properly decode data', async () => {
+      /* eslint-disable-next-line no-unused-vars */
+      const results = testVectors.map(([_, encoded]) => {
+        return [
+          decode(encoded),
+          encoded
+        ];
+      });
+      results.forEach((r, i) => {
+        should.exist(r);
+        should.exist(r[0], 'result should exist');
+        should.exist(r[1], 'expected result should exist');
+        r[0].should.equalBytes(testVectors[i][0]);
+        r[1].should.equal(testVectors[i][1]);
+      });
+    });
+    it('should fail to decode data', async () => {
+      invalidDecodeVectors.forEach(([input, expectedError]) => {
+        let result;
+        let error;
+        try {
+          result = decode(input);
+        } catch(e) {
+          error = e;
+        }
+        should.not.exist(result);
+        should.exist(error);
+        (error instanceof expectedError).should.be.true;
+      });
+    });
+  }); // end decode
+});
+
+function _toUint8Array(data) {
+  if(typeof data === 'string') {
+    // convert data to Uint8Array
+    return new TextEncoder().encode(data);
+  }
+  if(Array.isArray(data)) {
+    return new Uint8Array(data);
+  }
+  if(!(data instanceof Uint8Array)) {
+    throw new TypeError('"data" be a string, Array, or Uint8Array.');
+  }
+  return data;
+}
